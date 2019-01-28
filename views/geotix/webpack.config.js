029d26ff9cd29daca22eea850067569e28107268
Derek Graham <derek@mynorth.com>
2019-01-28 10:07:06 -0500

    Added Webpack and output

---
+++ views/geotix/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = [{
+  mode: 'production',
+  entry: './assets/scss/app.scss',
+  output: {
+    filename: './public/style-bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '../../../public/bundle.css',
+          },
+        },
+        { loader: 'extract-loader' },
+        { loader: 'css-loader' },
+        { loader: 'sass-loader' }
+      ]
+    }]
+  },
+}];
