4ad3752d5e9828525f648f245ba761b9fca1c66b
Russell Edens <rx@voomify.com>
2019-02-22 01:25:01 -0500

    Updated to work with global plugin.

---
+++ lib/geotix_presenter_plugin.rb
@@ -0,0 +1,1 @@
+p 'geotix styles loaded!'
--- lib/voom/presenters/plugins/geotix.rb
+++ lib/voom/presenters/plugins/geotix.rb
@@ -2,7 +2,16 @@ module Voom
   module Presenters
     module Plugins
       module Geotix
-
+        module WebClientComponents
+          # The string returned from this method will be added to the HTML header section of the page layout
+          # It will be called once for the page.
+          # The pom is passed along with the sinatra render method.
+          def render_header_geotix(_pom, render:)
+            p 'render_header_geotix'
+            view_dir = File.join(__dir__, 'geotix')
+            render.call :erb, :geotix_header, views: view_dir
+          end
+        end
       end
     end
   end
