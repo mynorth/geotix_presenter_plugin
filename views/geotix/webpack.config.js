57a839c1201dcfd184256300328859f28aedabf5
Russell Edens <rx@voomify.com>
2019-02-26 11:29:36 -0500

    Updated for the new MDC drawer and app bar (header).

--- views/geotix/webpack.config.js
+++ views/geotix/webpack.config.js
@@ -1,23 +1,28 @@
 module.exports = [{
-  mode: 'production',
-  entry: './assets/scss/app.scss',
-  output: {
-    filename: './public/style-bundle.js',
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '../../../public/bundle.css',
-          },
-        },
-        { loader: 'extract-loader' },
-        { loader: 'css-loader' },
-        { loader: 'sass-loader' }
-      ]
-    }]
-  },
+    mode: 'production',
+    entry: './assets/scss/app.scss',
+    output: {
+        filename: './public/style-bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '../../../public/bundle.css',
+                    },
+                },
+                {loader: 'extract-loader'},
+                {loader: 'css-loader'},
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: ['./node_modules']
+                    }
+                }
+            ]
+        }]
+    },
 }];
