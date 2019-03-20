a303a2beb1644574f4b7ad83d7e14fb1df4d382e
Russell Edens <rx@voomify.com>
2019-03-20 13:20:51 -0400

    Removed console print

--- lib/voom/presenters/plugins/geotix.rb
+++ lib/voom/presenters/plugins/geotix.rb
@@ -7,7 +7,6 @@ module Voom
           # It will be called once for the page.
           # The pom is passed along with the sinatra render method.
           def render_header_geotix(_pom, render:)
-            p 'render_header_geotix'
             view_dir = File.join(__dir__, 'geotix')
             render.call :erb, :geotix_header, views: view_dir
           end
